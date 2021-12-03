5cade4eeb8c0a9c80308640688c3c27f6b2e8057
rico-sakspari <gitfreder@gmail.com>
2021-12-04 02:03:58 +0700

    join table bagian review

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -3,9 +3,175 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Review;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Validator;
 
 class ReviewController extends Controller
 {
-    //
+    public function userRoomReview($room_id,$user_id) //parameterb id kamar
+    {
+        //ambil data review dari kamar tertentu
+        $roomReviews = DB::table('reviews')
+            ->where('room_id','=',$room_id)
+            ->join('users','reviews.user_id','=','users.id')
+            ->where('users.id',$user_id)
+            ->get();
+
+
+        if(count($roomReviews)>0){
+            return response([
+                'message' => 'Retrive All Success',
+                'data' => $roomReviews
+            ], 200);
+        }
+        return response([
+            'message' => 'No Review for this room',
+            'data' => null
+        ], 400); //return message data kosong
+    }
+
+    public function reviewWithUser($id) //parameterb id kamar
+    {
+        //ambil data review dari kamar tertentu
+        $roomReviews = DB::table('reviews')
+            ->where('room_id','=',$id)
+            ->leftJoin('users','reviews.user_id','=','users.id')
+            ->get();
+
+
+        if(count($roomReviews)>0){
+            return response([
+                'message' => 'Retrive All Success',
+                'data' => $roomReviews
+            ], 200);
+        }
+        return response([
+            'message' => 'No Review for this room',
+            'data' => null
+        ], 400); //return message data kosong
+    }
+
+
+
+//    public function userReviewRoom($id_kamar,$id_user)
+//    {
+//        //ambil data review dari kamar tertentu
+//        $roomReviews = DB::table('reviews')
+//            ->where('room_id','=',$id_kamar) ->get();
+//
+//
+//        if(count($roomReviews)>0){
+//            return response([
+//                'message' => 'Retrive All Success',
+//                'data' => $roomReviews
+//            ], 200);
+//        }
+//        return response([
+//            'message' => 'No Review for this room',
+//            'data' => null
+//        ], 400); //return message data kosong
+//    }
+
+    public function show($id)
+    {
+        $review = Review::find($id); //mencari booking detail berdasarkan data id
+
+        if (!is_null($review)) {
+            return response([
+                'message' => 'Retrive All Success',
+                'data' => $review
+            ], 200);
+        }
+
+        return response([
+            'message' => 'Review Not Found',
+            'data' => null
+        ], 404);
+    }
+
+//    method untuk menambah booking baru
+    public function store(Request $request)
+    {
+        $storeData = $request->all();
+        $validate = Validator::make($storeData, [
+            'room_id' => 'required',
+            'user_id' => 'required',
+            'review_date' => 'required|date',
+            'description' => 'required',
+        ]); // validasi inputan
+
+        if ($validate->fails()) {
+            return response(['message' => $validate->errors()], 400);
+        }
+
+        $review = Review::create($storeData);
+        return response([
+            'message' => 'Add Review Success',
+            'data' => $review
+        ], 200); //return data review dalam bentuk JSON
+    }
+
+//    method untuk menghapus data review tertentu
+    public function destroy($id)
+    {
+        $review = Review::find($id);
+
+        if(is_null($review)){
+            return response([
+                'message'=>'Review Not Found',
+                'data'=>null
+            ],400);
+        }//return message saat database tidak ditemukan
+
+        if($review->delete()){
+            return response([
+                'message'=>'Delete Review Success',
+                'data'=>$review
+            ],200);
+        }
+
+        return response([
+            'message'=>'Delete Booking Failed',
+            'data'=>null
+        ],400);
+    }
+
+    public function update(Request $request, $id){
+        $review = Review::find($id);
+        if(is_null($review)){
+            return response([
+                'message'=>'Review Not Found',
+                'data'=>null
+            ],400);
+        }
+
+        $updateData = $request->all();
+        $validate = Validator::make($updateData, [
+            'room_id' => 'required',
+            'user_id' => 'required',
+            'review_date' => 'required|date',
+            'description' => 'required',
+        ]); // validasi data
+
+        if ($validate->fails()) {
+            return response(['message' => $validate->errors()], 400);
+        }
+        $review->room_id = $updateData['room_id'];
+        $review->user_id = $updateData['user_id'];
+        $review->review_date = $updateData['review_date'];
+        $review->description = $updateData['description'];
+
+        if($review->save()){
+            return response([
+                'message'=> 'Update Review Success',
+                'data'=>$review
+            ],200);
+        }
+        return response([
+            'message'=>'Update Review Failed',
+            'data'=>null
+        ],400);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,10 @@ Route::get('booking/{id}','Api\BookController@show'); //ambil data booking terte
 Route::post('booking','Api\BookController@store'); //simpan data booking tertentu
 Route::put('booking/{id}','Api\BookController@update'); //update data booking tertentu
 Route::delete('booking/{id}','Api\BookController@destroy'); //update data booking tertentu
+
+Route::get('user-room-reviews/{room_id}/{user_id}','Api\ReviewController@userRoomReview'); //ambill data review dari room tertentu
+Route::get('room-reviews/{id}','Api\ReviewController@reviewWithUser'); //ambill data review dari room tertentu
+Route::get('review/{id}','Api\ReviewController@show'); //ambil data review tertentu
+Route::post('review','Api\ReviewController@store'); //simpan data review tertentu
+Route::put('review/{id}','Api\ReviewController@update'); //update data booking tertentu
+Route::delete('review/{id}','Api\ReviewController@destroy'); //update data booking tertentu
