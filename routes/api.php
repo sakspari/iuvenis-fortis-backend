2d6ac6034cff8bf11f9e7e2f0cdaaa3312af6d49
rico-sakspari <gitfreder@gmail.com>
2021-12-04 02:09:54 +0700

    merge branch controller dengan branch terang

--- routes/api.php
+++ routes/api.php
@@ -18,14 +18,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('user-bookings/{id}','Api\BookController@index'); //dipakai di review your room
+Route::get('user-bookings/{id}','Api\BookController@index'); //parameter id user
 Route::get('booking/{id}','Api\BookController@show'); //ambil data booking tertentu
 Route::post('booking','Api\BookController@store'); //simpan data booking tertentu
 Route::put('booking/{id}','Api\BookController@update'); //update data booking tertentu
 Route::delete('booking/{id}','Api\BookController@destroy'); //update data booking tertentu
 
-Route::get('user-room-reviews/{room_id}/{user_id}','Api\ReviewController@userRoomReview'); //ambill data review dari room tertentu
-Route::get('room-reviews/{id}','Api\ReviewController@reviewWithUser'); //ambill data review dari room tertentu
+Route::get('user-room-reviews/{room_id}/{user_id}','Api\ReviewController@userRoomReview'); //ambill data review dari room tertentu yang dibuat oleh user tertentu
+Route::get('room-reviews/{id}','Api\ReviewController@reviewWithUser'); //ambill data review dari room tertentu dari semua user
 Route::get('review/{id}','Api\ReviewController@show'); //ambil data review tertentu
 Route::post('review','Api\ReviewController@store'); //simpan data review tertentu
 Route::put('review/{id}','Api\ReviewController@update'); //update data booking tertentu
