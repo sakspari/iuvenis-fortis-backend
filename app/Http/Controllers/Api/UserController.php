5889578107ec6eb457a43f7c6536d56322388830
rico-sakspari <gitfreder@gmail.com>
2021-12-07 06:49:18 +0700

    update user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -39,7 +39,7 @@ class UserController extends Controller
         if (!is_null($users)) {
             return response([
                 'message' => 'Retrieve User Success',
-                'data' => [$users]
+                'data' => $users
             ], 200);
         }
 
