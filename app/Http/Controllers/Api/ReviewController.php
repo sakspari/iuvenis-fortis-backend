7ebaf2fa6317edea571da425875b3ec0a4214630
rico-pc <gitfreder@gmail.com>
2021-12-19 11:52:55 +0700

    room reviews

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -35,9 +35,13 @@ class ReviewController extends Controller
     public function reviewWithUser($id) //parameterb id kamar
     {
         //ambil data review dari kamar tertentu
+//        $roomReviews = DB::table('reviews')
+//            ->where('room_id','=',$id)
+//            ->leftJoin('users','reviews.user_id','=','users.id')
+//            ->get();
+
         $roomReviews = DB::table('reviews')
             ->where('room_id','=',$id)
-            ->leftJoin('users','reviews.user_id','=','users.id')
             ->get();
 
 
